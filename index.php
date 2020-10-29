d0db5eb7256f71238fdaf8bc92296f272a400743
Vijay Kumar <62456443+therealvkr@users.noreply.github.com>
2020-10-30 01:17:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,11 @@ $llocf = urlencode($video_id);
   if (empty($llocf)){
   echo '<html lang="en">
 <head>
-  <title>DLvkr</title>
-    <link rel="icon" href="suramshivareddy.com/photo.png" type="image/gif">
-    <meta name="author" content="Suram ShivaReddy" />
+  <title>DLvkr : PHP video & audio downloader from youtube and 1000+ sites ..</title>
+    <link rel="icon" href="/logo.png" type="image/png">
+    <meta name="author" content="theofficialvkr" />
     <meta name="keywords"  content="DLvkr" />
-<meta name="description" content="Im Suram ShivaReddy a Webdeveloper from India (Hyderabad). If you are looking for a
-good looking Responsive website, I can help you with it">
+<meta name="description" content="DLvkr : PHP video & audio downloader from youtube and 1000+ sites ..">
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -44,7 +43,7 @@ cursor:pointer;
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">
-   <p style="text-align:center;color:black;margin-top:-5px;"class="lead"> Youtube Downloader</p>
+   <p style="text-align:center;color:black;margin-top:-5px;"class="lead"> DLvkr</p>
       </a>
     </div>
     <form  id="headerform" method="GET" action="/info" style="border:none;border-shadow:none;" class="navbar-form navbar-right" role="search">
@@ -59,18 +58,127 @@ cursor:pointer;
 <div class="container">
 
   <div class="jumbotron">
-    <h1 style=" text-align:center;"><i style="color:#d9534f;"class="fa  fa-2x fa-youtube" aria-hidden="true"></i> Downloader</h1>
-    <p align="center" class="lead">Get Youtube videos to your Devices !</p> <br>
+    <h1 style=" text-align:center;"><i style="color:#d9534f;"class="fa  fa-2x fa-vimeo" aria-hidden="true"></i> Downloader</h1>
+    <p align="center" class="lead">Get Any videos to your Devices !</p> <br>
         <form  role="search" method="GET" action="/info">
   <div class="form-group">
     <input type="text"  class="form-control" name="sdl"placeholder="Search or Paste a Youtube Link">
   </div>
   <button type="submit"class="btn btn-danger btn-block">Submit</button>
 </form>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+</style>
+</head>
+<body>
+
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="https://dlvnow.herokuapp.com/logo.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>AnyThing</h2>
+        <p class="title">AnyThing</p>
+        <p>Put AnyThing Here.</p>
+        <p>example@example.com</p>
+        <p><a href="https://facebook.com/theofficialvkr"><button class="button">Facebook</button></a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://dlvnow.herokuapp.com/logo.png" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>AnyThing</h2>
+        <p class="title">AnyThing</p>
+        <p>Put AnyThing Here.</p>
+        <p>example@example.com</p>
+        <p><a href="https://twitter.com/theofficialvkr"><button class="button">Twitter</button></a></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <img src="https://dlvnow.herokuapp.com/logo.png" alt="John" style="width:100%">
+      <div class="container">
+        <h2>AnyThing</h2>
+        <p class="title">AnyThing</p>
+        <p>Put AnyThing Here.</p>
+        <p>example@example.com</p>
+        <p><a href="https://instagram.com/theofficialvkr"><button class="button">instagram</button></a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
   </div>
 
 <footer style=" height:30px; margin-top:10px;background-color:#F8F8F8;width: 100%;">
-<p style="padding-top:5px;" align="center">Production - <a href="https://www.facebook.com/shivareddy0">Shiva Rdy</a> </p>
+<p style="padding-top:5px;" align="center">Powred By - <a href="https://www.facebook.com/theofficialvkr"> Vijay Kumar </a> </p>
 </footer>
 </div>
 
