b9778690c0db50ee84587d50ab315e67da5e16f0
Vijay Kumar <62456443+therealvkr@users.noreply.github.com>
2021-01-18 01:12:04 +0530

    Add files via upload

--- sf.php
+++ sf.php
@@ -1,18 +1,15 @@
-<?php error_reporting(0);
+<?php error_reporting(0); ?>
+<?php
 $urlx = $_SERVER['REQUEST_URI'];
 $video_id = explode("?sdl=", $urlx);
 $video_id = $video_id[1];
 $lloc = urldecode($video_id);
-$llocf = urlencode($video_id);
-
-if(stripos($video_id,'://')==true) 
-{
-    header("Location: /info/?sdl=$lloc");
-    exit();
-  } 
-
-
-   else {
-    header("Location: /info/search.php?sdl=$lloc");
-    exit();
-  } 
+$llocf = urldecode($video_id);  
+ if(stripos($lloc,'://')===false)  {
+      header("Location: /info/search.php/?sdl=$lloc");
+          exit();
+           
+            }
+               else header("Location: /info/?sdl=$lloc");
+                   exit();
+                   ?>
